e2346c50bab57ba1fdd6cab10eb764d94c5a3479
Vitor Fernandes <vitor.fernandes@blip.pt>
2016-02-29 10:28:54 +0000

    Added retrieval part for the access token

--- butils/ereader/goauth.py
+++ butils/ereader/goauth.py
@@ -37,3 +37,18 @@ authorize_link = '%s?oauth_token=%s' % (authorize_url,
 print "Use a browser to visit this link and accept your application:"
 print authorize_link
 accepted = 'n'
+
+while accepted.lower() == 'n':
+    # you need to access the authorize_link via a browser,
+    # and proceed to manually authorize the consumer
+    accepted = raw_input('Have you authorized me? (y/n) ')
+
+token = oauth.Token(request_token['oauth_token'],
+                    request_token['oauth_token_secret'])
+
+client = oauth.Client(consumer, token)
+response, content = client.request(access_token_url, 'POST')
+if response['status'] != '200':
+    raise Exception('Invalid response: %s' % response['status'])
+
+access_token = dict(urlparse.parse_qsl(content))
