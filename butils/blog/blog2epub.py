6fcab3ff0162b259c192e744a1c2b52f7e72ad60
balhau <balhau@balhau.net>
2016-04-01 15:33:26 +0100

    Added new format for argparse documentation

--- butils/blog/blog2epub.py
+++ butils/blog/blog2epub.py
@@ -13,6 +13,9 @@ def main():
 
     DESCRIPTION='''
         This script will enable you to export your blog into epub. For now several blog engines are supported. Namely blogspot, wordpress, and ghost
+        Examples:
+            To extract a wordpress blog into an epub you use, for example, the following
+                ./blog2epub.py -b wordpress -l pt  -a "Balhau" -u "https://blog.balhau.net/" -n "Blog do Balhau.epub"
     '''
 
     GHOST='ghost'
@@ -20,34 +23,46 @@ def main():
     WORDPRESS='wordpress'
 
     try:
-        parser = argparse.ArgumentParser(description=DESCRIPTION)
+        parser = argparse.ArgumentParser(description=DESCRIPTION,formatter_class=argparse.RawTextHelpFormatter)
 
         parser.add_argument('-b','--blog',
-            help='The blog provider name, for now possible values are <ghost>, <blogger> and <wordpress>')
+            help='''
+            The blog provider name, for now possible values are <ghost>, <blogger> and <wordpress>
+            ''')
 
         parser.add_argument('-d','--database',
-            help='Use this parameter only when dealing with blogs based on ghost engine to specify the path for the sqlite database')
+            help='''
+            Use this parameter only when dealing with blogs based on ghost engine to specify the path for the sqlite database
+            ''')
 
-        parser.add_argument('-u','--url', help='This receives de url of the blog, use this for blogs based on blogger and wordpress')
+        parser.add_argument('-u','--url', help='''
+            This receives de url of the blog, use this for blogs based on blogger and wordpress
+        ''')
 
         parser.add_argument('-a','--authors',
-            help='List of authors. You should use a comma to split the several entries. \
-        For example if your list has two authors namely Steven John and Charles Pryo you should pass them like this -a \
-        "Charles Pryo,Steven John" and don\'t forget the " in the beginning and end of the list')
+            help='''
+            List of authors. You should use a comma to split the several entries.
+            For example if your list has two authors namely Steven John and Charles Pryo you should pass them like this -a
+            Charles Pryo,Steven John" and don\'t forget the " in the beginning and end of the list
+            ''')
 
         parser.add_argument('-o','--output',
-            help='This is the name that will be assigned for the file that will be generated.')
+            help='''
+            This is the name that will be assigned for the file that will be generated.
+            ''')
 
         parser.add_argument('-n','--name',
-            help='This is the name of the blog, it is mandatory for blogger and worpress but not used in ghost blogs \
-            since it is auto detected in this last case. It will be the field used for the title of the ebook in the \
-            final output file'
-        )
+            help='''
+            This is the name of the blog, it is mandatory for blogger and worpress but not used in ghost blogs
+            since it is auto detected in this last case. It will be the field used for the title of the ebook in the
+            final output file
+            ''')
 
         parser.add_argument('-l','--language',
-            help='Epub file needs to set a language to help ereaders and other devices to decode in the most proper way the \
-            epub file'
-        )
+            help='''
+            Epub file needs to set a language to help ereaders and other devices to decode in the most proper way the
+            epub file
+            ''')
 
         args = parser.parse_args()
 
@@ -62,11 +77,11 @@ def main():
 
         #Process wordpress request
         if args.blog == 'wordpress':
-            print "process wordpress"
             auths=args.authors.split(",")
             lang=args.language
             blogHost=args.url
             ebookName=args.name
+            print "Extracting from Wordpress: ",blogHost," into ", ebookName
             wp=Wordpress(blogHost,ebookName,language=lang,authors=auths)
             wp.toEpub(ebookName)
 
