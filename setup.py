99a4ccacbeba3c6c70ef0bdcdd9add2522235431
balhau <balhau@balhau.net>
2016-04-02 19:32:26 +0100

    Added blog2epub into a binary folder fixed setup script

--- setup.py
+++ setup.py
@@ -9,15 +9,29 @@ pname="butils"
 #packages=[pname]+[pname+ "." + name
 #	for name in os.listdir(os.path.join(pname)) if os.path.isdir(os.path.join(pname, name))]
 
+packages=[pname]+[pname+ "." + name
+	for name in os.listdir(os.path.join(pname)) if os.path.isdir(os.path.join(pname, name))]
+
 current_dir=os.path.dirname(__file__)
 
 with codecs.open(os.path.join(current_dir,'README.md'),'r','utf8') as readme_file:
 		long_description=readme_file.read()
 
-setup(	name='butils',
-	version='1.0.10',
+setup(
+	name=pname,
+	author='Balhau',
+	author_email='balhau@balhau.net',
+	platforms='any',
+	version='1.0.13',
 	url='https://git.balhau.net/',
 	license='MIT License',
+	scripts=['bin/blog2epub'],
+	include_package_data = True ,
 	description='A bunch of utility scripts with balhau hacks',
-	long_description=long_description,
-	packages=find_packages())
+	long_description=__doc__,
+	packages=packages,
+	install_requires=[
+		'ebooklib',
+		'bs4'
+	]
+)
